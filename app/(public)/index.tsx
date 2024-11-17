502ed844810245ebbe4b2c105fbf183493d0b942
nazhoz <chemmalaniyas1145@gmail.com>
2024-11-17 18:28:49 +0530

    clerk js

--- app/(public)/index.tsx
+++ app/(public)/index.tsx
@@ -1,23 +1,41 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useOAuth } from "@clerk/clerk-expo";
+import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_facebook" });
+  const { startOAuthFlow: startGoogle0AuthFlow } = useOAuth({
+    strategy: "oauth_google",
+  });
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hey Developer!</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.loginImage}
+        source={require("@/assets/images/login.png")}
+      />
+      <ScrollView>
+      <Text style={styles.title}>How would you like to use this app ?</Text>
+      <TouchableOpacity>
+        <Text>Continue with facebook</Text>
+      </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent: 'center',
-    alignItems:'center'
+  container: {
+    flex: 1,
+    gap: 20,
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+  loginImage: {
+    width: "100%",
+    height: 350,
+    resizeMode: "contain",
+  },
+  title:{
+    fontFamily: 'DMSans_700Bold',
+    fontSize: 17,
   }
-})
+});
